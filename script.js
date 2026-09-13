3124d2a6d739bb57b37b1d6bf91d245849b586da
ttt913681-prog <ttt913681@gmail.com>
2026-09-13 21:57:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwRYTofrqiijrsYg2A-hIBflqWr6Lpkg-v9ejn0RpuzxNztDGyowtHiinOZMD1flxoC/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxZYLuhEyAMkVgNYrTDBJmQQkP00vXBvodq7Q39Rp4XY0GBtEBKjwfcWJXjn2Z7l75a/exec';
 const CSV_URL = 'YOUR_CSV_URL';
 
 document.addEventListener('DOMContentLoaded', () => {
